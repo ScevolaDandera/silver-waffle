dcacd86f6d1d0a9ca26c10e22af5eeb1e6dc13af
ScevolaDandera <rashim900@gmail.com>
2022-01-18 19:47:14 +0000

    displacement segments not same with bumps

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { THREE, Project, Scene3D, PhysicsLoader } from 'enable3d'
+import { THREE, Project, Scene3D, PhysicsLoader, ThirdDimension, ExtendedMesh } from 'enable3d'
 
 class MainScene extends Scene3D {
   constructor() {
@@ -16,43 +16,56 @@ class MainScene extends Scene3D {
 
   async create() {
     // set up scene (light, ground, grid, sky, orbitControls)
-     let { camera,  ground, lights, orbitalControls } = await this.warpSpeed()
-   //  console.log(ground);
+   let { camera, lights, orbitalControls } = await this.warpSpeed('-ground')
+  //     let { camera, lights, ground, orbitalControls } = await this.warpSpeed()
+  // console.log(ground);
+   this.physics.debug.enable();
+  
+     const planeTexture = await this.load.texture(
+      "/static/rock.webp"
+    );
 
-     const sphereGeometry = new THREE.PlaneGeometry( 1, 1, 10,10 );
-     const pmaterial = new THREE.MeshStandardMaterial( {
-      color: 'grey',
-      wireframe: true,
+    const heightmap = await this.load.texture(
+      "/static/heightmap.png"
+    );
+
+
+
+
+    //
+    const geometry = new THREE.PlaneBufferGeometry( 20, 20, 10, 10);
+    const material = new THREE.MeshStandardMaterial( {
+     map: planeTexture,
+   //  displacementMap: heightmap,
+  //   displacementScale: 4
+   //  wireframe: true
+    //  alphaMap: alphamap,
+    // side: THREE.DoubleSide,
+      // transparent: true, 
+      // depthTest: false
   } );
-  ground = new THREE.Mesh(sphereGeometry, pmaterial);
- // this.physics.add.ground(this.newground);
-  // this.ground.material = pmaterial;
-  // this.ground.geometry = sphereGeometry;
+    const plane = this.heightMap.add(heightmap);
+ //   const plane = new ExtendedMesh(geometry, material)
+    plane.material = material;
+    plane.geometry = geometry;
+    plane.position.set(0, 0, 0)
+   plane.rotation.x =  -Math.PI / 2;
+    this.scene.add(plane)
+    this.physics.add.existing(plane, { shape: 'convex', mass: 0, collisionFlags:2})
 
-
-
-
-
-
-
-     
-    // enable physics debug
-    this.physics.debug.enable()
 
     // position camera
-    this.camera.position.set(5, 5, 10)
-    // blue box (without physics)
-   this.staticbox = this.physics.add.box({ y: 5 }, { lambert: { color: 'deepskyblue' } })
+    this.camera.position.set(5, 15, 50)
 
     // pink box (with physics)
-   this.phyicsbox =  this.physics.add.box({ y: 0 }, { lambert: { color: 'hotpink' } })
-   this.phyicsbox2 =  this.physics.add.box({ z: 0, x:-10 }, { lambert: { color: 'yellow' } })
-   this.phyicsbox2.body.applyForceX(5);
+   this.phyicsbox =  this.physics.add.box({ y: 10 }, { lambert: { color: 'hotpink' } })
+   this.phyicsbox2 =  this.physics.add.box({ y: 20, z: 0, x:5 }, { lambert: { color: 'yellow' } })
+   this.phyicsbox2.body.applyForceX(1);
   }
 
   update() {
-    this.staticbox.rotation.x += 1;
-    this.phyicsbox.rotation.y += 1;
+    // this.staticbox.rotation.x += 1;
+    // this.phyicsbox.rotation.y += 1;
   }
 }
 
